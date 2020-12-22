4bcfede1076bef0066c5d1e4e30134cd5176a70b
vipulmore12 <vipulmore1998@gmail.com>
2020-12-22 20:07:15 +0530

    Daily wage

--- EmpWageComp.java
+++ EmpWageComp.java
@@ -3,10 +3,16 @@ public class EmpWageComp
 	public static void main (String[] args)
 	{
 		int IS_FULL_TIME=1;
+		int WAGE_PER_HOUR=20;
+		int FULL_DAY_HOUR=8;
+		int dailyWage=0;
+		int employee_hours=0;
 		double empCheck=Math.floor(Math.random()*10)%2;
 		if(empCheck==IS_FULL_TIME)
 			System.out.println("Employee is present");
 		else
 			System.out.println("Employee is Absent");
+		dailyWage=(employee_hours*WAGE_PER_HOUR);
+		System.out.println("Daily Wage:"+dailyWage);
 	}
 }
