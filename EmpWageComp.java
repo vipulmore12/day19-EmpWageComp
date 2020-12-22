03b2c147b6673374b92f42b998df7df7f82a92b9
vipulmore12 <vipulmore1998@gmail.com>
2020-12-22 20:00:26 +0530

    Employee Attendance

---
+++ EmpWageComp.java
@@ -0,0 +1,12 @@
+public class EmpWageComp
+{
+	public static void main (String[] args)
+	{
+		int IS_FULL_TIME=1;
+		double empCheck=Math.floor(Math.random()*10)%2;
+		if(empCheck==IS_FULL_TIME)
+			System.out.println("Employee is present");
+		else
+			System.out.println("Employee is Absent");
+	}
+}
